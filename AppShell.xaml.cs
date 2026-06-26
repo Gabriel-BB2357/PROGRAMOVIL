fc38aa9704911b3c5b89f61f6490240627499c0a
Gabriel-BB2357 <barriosg2357@gmail.com>
2026-06-25 18:47:04 -0600

    primer commit subida del proyecto conectado a firebase

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CRadventure
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Plugin.Firebase;
+using Plugin.Firebase.Auth;
+namespace CRadventure;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+
+
+
+    private async void Registrar_Clicked(
+        object sender,
+        EventArgs e)
+    {
+        try
+        {
+            var auth = CrossFirebaseAuth.Current;
+
+            var result =
+                await auth.CreateUserAsync(
+                    txtCorreo.Text,
+                    txtPassword.Text);
+
+            await DisplayAlert(
+                "Éxito",
+                $"Usuario creado: {result.Email}",
+                "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert(
+                "Error",
+                ex.Message,
+                "OK");
+        }
+    }
+
+    private async void Login_Clicked(
+        object sender,
+        EventArgs e)
+    {
+        try
+        {
+            var auth = CrossFirebaseAuth.Current;
+
+            var user =
+                await auth.SignInWithEmailAndPasswordAsync(
+                    txtCorreo.Text,
+                    txtPassword.Text);
+
+            await Navigation.PushAsync(
+                new DashboardPage(user.Email));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert(
+                "Error Login",
+                ex.Message,
+                "OK");
+        }
+    }
+}
